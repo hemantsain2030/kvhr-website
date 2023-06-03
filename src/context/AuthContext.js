6ddaa503936df37c757d99875d9b8551c9be6683
Hemant Sain <hemantsain2030@gmail.com>
2023-06-03 12:51:13 +0530

    Second commit Login and Registration portals

---
+++ src/context/AuthContext.js
@@ -0,0 +1,39 @@
+// import React, {useContext, useState, useEffect} from 'react';
+// import {auth} from '../firebase'
+
+
+
+// const AuthContext = React.createContext()
+
+// export function useAuth(){
+//     return useContext(AuthContext)
+// }
+
+// export function AuthProvider({children}) {
+//     const [currentUser, setCurrentUser] = useState()
+
+//     function signup(email, password){
+//         return auth.createUserWitEmailAndPassword(email, password)
+//     }
+
+
+//     useEffect(()=>{
+//        const unsubscribe = auth.onAuthStateChanged(function(user){
+//             setCurrentUser(user)
+//         })
+
+//         return unsubscribe
+//     }, [])
+
+  
+
+//     const value ={
+//         currentUser,
+//         signup
+//     }
+//   return (
+//     <AuthContext.Provider value={value}>
+//         {children}
+//     </AuthContext.Provider>
+//   );
+// }
